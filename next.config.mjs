c4adf7daa8ead4c16c890e719776e405ebb5d858
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2024-02-14 08:49:08 -0700

    docs: fixing workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  // Add basePath
+  basePath: '/portfolio',
+};
 
 export default nextConfig;
